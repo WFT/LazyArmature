42cf6d26633fa1dfbdbf1f03ce3ae00d07327ce6
WFT <will.a.ft@gmail.com>
2014-06-10 22:31:32 -0400

    monolithicality

--- src/Display.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
-module Display where
-
-import Foreign.C
-
-foreign import ccall "set_screen" setScreen :: CDouble -> CDouble -> CDouble -> CDouble -> IO ()
-
-foreign import ccall "init_live_render" initDisplay :: CInt -> CInt -> IO CChar
