f464527d3c9d72b36437f91059c8df85764a8a0d
WFT <will.a.ft@gmail.com>
2014-06-12 12:44:38 -0400

    named data structure

--- src/Bone.hs
+++ src/Bone.hs
@@ -1,6 +1,17 @@
+module Bones where
+
 import Import
 import Foreign.Ptr
+import Foreign.C
 
-data Joint = Bone Int Int Int
-data Bone = Lig Joint (Ptr Matrix) Joint
+data Joint = Bone { x :: CDouble
+                  , y :: CDouble
+                  , z :: CDouble
+                  }
+
+data Bone = Lig { start :: Joint
+                , mesh :: (Ptr Matrix)
+                , color :: (Ptr Matrix)
+                , end :: Joint
+                }
 
